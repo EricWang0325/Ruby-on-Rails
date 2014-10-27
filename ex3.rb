6d7a5dddd353059861738ae3b15c47b4b2f572fd
EricWang0325 <eric19910325@hotmail.com>
2014-10-28 04:11:40 +0800

    Ruby homework_1

---
+++ ex3.rb
@@ -0,0 +1,23 @@
+# This is a comment.
+puts "I will now count my chickens:"
+# This is an equation.
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 * 0.04}"
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 * 0.02 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+# This is a question.
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+puts "How about some more."
+# Additional Results
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
